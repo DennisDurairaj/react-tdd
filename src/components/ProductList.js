f9df9258829461f095e2a94d459469af337a1d6d
Dennis Durairaj <dennis.durairaj@gmail.com>
2018-05-23 18:51:25 +0200

    Initial commit

---
+++ src/components/ProductList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ProductList(props) {
+  return <div>ProductList</div>;
+}
+
+ProductList.propTypes = {
+  products: PropTypes.array.isRequired
+};
+export default ProductList;
